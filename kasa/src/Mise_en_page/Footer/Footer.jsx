3c09fcdc793b448e4f760e9454c53db6b66e1621
Romy0110 <romydev01@gmail.com>
2023-07-23 13:43:19 +0200

    ajout des logos footer et header

--- kasa/src/Mise_en_page/Footer/Footer.jsx
+++ kasa/src/Mise_en_page/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../css/style.css";
-
+import logoFooter from "../../assets/images/Logos/LogoFooter.png";
 
 function Footer() {
     return(
         <footer>
             <div id="footer-container">
-                <img alt="Logo" />
+                <img src={logoFooter} alt="Logo" />
                 <p>&copy; 2022 Kasa. Tous droits réservés</p>
             </div>
         </footer>
--- kasa/src/Mise_en_page/Header/Header.jsx
+++ kasa/src/Mise_en_page/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../css/style.css";
-
+import logoHeader from "../../assets/images/Logos/LogoHeader.png";
 import { NavLink } from "react-router-dom";
 
 function Header() {
     return(
         <header>
-            <img  alt="Logo"/>
+            <img src={logoHeader} alt="Logo"/>
             <nav>
                 <NavLink to="/Accueil" className="lien-site lien-menu">Accueil</NavLink>
                 <NavLink to="/About" className="lien-site lien-menu">À propos</NavLink>
